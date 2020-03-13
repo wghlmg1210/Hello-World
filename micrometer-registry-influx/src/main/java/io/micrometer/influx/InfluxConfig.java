5674001839e7c9113e0513073a6201dff960af0e
xb.huang <xb.huang@shunwang.com>
2020-03-13 09:47:41 +0800

    none

---
+++ micrometer-registry-influx/src/main/java/io/micrometer/influx/InfluxConfig.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2017 Pivotal Software, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.influx;
+
+import io.micrometer.core.instrument.step.StepRegistryConfig;
+import io.micrometer.core.lang.Nullable;
+
+/**
+ * Configuration for {@link InfluxMeterRegistry}.
+ *
+ * @author Jon Schneider
+ */
+public interface InfluxConfig extends StepRegistryConfig {
+    /**
+     * Accept configuration defaults
+     */
+    InfluxConfig DEFAULT = k -> null;
+
+    @Override
+    default String prefix() {
+        return "influx";
+    }
+
+    /**
+     * @return The db to send metrics to. Defaults to "mydb".
+     */
+    default String db() {
+        String v = get(prefix() + ".db");
+        return v == null ? "mydb" : v;
+    }
+
+    /**
+     * @return Sets the write consistency for each point. The Influx default is 'one'. Must
+     * be one of 'any', 'one', 'quorum', or 'all'. Only available for InfluxEnterprise clusters.
+     */
+    default InfluxConsistency consistency() {
+        String v = get(prefix() + ".consistency");
+        if (v == null)
+            return InfluxConsistency.ONE;
+        return InfluxConsistency.valueOf(v.toUpperCase());
+    }
+
+    /**
+     * @return Authenticate requests with this user. By default is {@code null}, and the registry will not
+     * attempt to present credentials to Influx.
+     */
+    @Nullable
+    default String userName() {
+        return get(prefix() + ".userName");
+    }
+
+    /**
+     * @return Authenticate requests with this password. By default is {@code null}, and the registry will not
+     * attempt to present credentials to Influx.
+     */
+    @Nullable
+    default String password() {
+        return get(prefix() + ".password");
+    }
+
+    /**
+     * @return Influx writes to the DEFAULT retention policy if one is not specified.
+     */
+    @Nullable
+    default String retentionPolicy() {
+        return get(prefix() + ".retentionPolicy");
+    }
+
+    /**
+     * @return Time period for which influx should retain data in the current database (e.g. 2h, 52w).
+     */
+    @Nullable
+    default String retentionDuration() {
+        return get(prefix() + ".retentionDuration");
+    }
+
+    /**
+     * @return How many copies of the data are stored in the cluster. Must be 1 for a single node instance.
+     */
+    @Nullable
+    default Integer retentionReplicationFactor() {
+        String v = get(prefix() + ".retentionReplicationFactor");
+        return v == null ? null : Integer.valueOf(v);
+    }
+
+    /**
+     * @return The time range covered by a shard group (e.g. 2h, 52w).
+     */
+    @Nullable
+    default String retentionShardDuration() {
+        return get(prefix() + ".retentionShardDuration");
+    }
+
+
+    /**
+     * @return The URI for the Influx backend. The default is {@code http://localhost:8086}.
+     */
+    default String uri() {
+        String v = get(prefix() + ".uri");
+        return (v == null) ? "http://localhost:8086" : v;
+    }
+
+    /**
+     * @return {@code true} if metrics publish batches should be GZIP compressed, {@code false} otherwise.
+     */
+    default boolean compressed() {
+        String v = get(prefix() + ".compressed");
+        return v == null || Boolean.parseBoolean(v);
+    }
+
+    /**
+     * @return {@code true} if Micrometer should check if {@link #db()} exists before attempting to publish
+     * metrics to it, creating it if it does not exist.
+     */
+    default boolean autoCreateDb() {
+        String v = get(prefix() + ".autoCreateDb");
+        return v == null || Boolean.parseBoolean(v);
+    }
+}
---
+++ micrometer-registry-influx/src/main/java/io/micrometer/influx/InfluxMeterRegistry.java
@@ -0,0 +1,310 @@
+/**
+ * Copyright 2017 Pivotal Software, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.influx;
+
+import io.micrometer.core.instrument.*;
+import io.micrometer.core.instrument.distribution.HistogramSupport;
+import io.micrometer.core.instrument.distribution.ValueAtPercentile;
+import io.micrometer.core.instrument.step.StepMeterRegistry;
+import io.micrometer.core.instrument.util.*;
+import io.micrometer.core.ipc.http.HttpSender;
+import io.micrometer.core.ipc.http.HttpUrlConnectionSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+
+/**
+ * {@link MeterRegistry} for InfluxDB.
+ *
+ * @author Jon Schneider
+ * @author Johnny Lim
+ */
+public class InfluxMeterRegistry extends StepMeterRegistry {
+    private static final ThreadFactory DEFAULT_THREAD_FACTORY = new NamedThreadFactory("influx-metrics-publisher");
+    private final InfluxConfig config;
+    private final HttpSender httpClient;
+    private final Logger logger = LoggerFactory.getLogger(InfluxMeterRegistry.class);
+    private boolean databaseExists = false;
+
+    @SuppressWarnings("deprecation")
+    public InfluxMeterRegistry(InfluxConfig config, Clock clock) {
+        this(config, clock, DEFAULT_THREAD_FACTORY,
+                new HttpUrlConnectionSender(config.connectTimeout(), config.readTimeout()));
+    }
+
+    /**
+     * @param config        Configuration options for the registry that are describable as properties.
+     * @param clock         The clock to use for timings.
+     * @param threadFactory The thread factory to use to create the publishing thread.
+     * @deprecated Use {@link #builder(InfluxConfig)} instead.
+     */
+    @Deprecated
+    public InfluxMeterRegistry(InfluxConfig config, Clock clock, ThreadFactory threadFactory) {
+        this(config, clock, threadFactory, new HttpUrlConnectionSender(config.connectTimeout(), config.readTimeout()));
+    }
+
+    private InfluxMeterRegistry(InfluxConfig config, Clock clock, ThreadFactory threadFactory, HttpSender httpClient) {
+        super(config, clock);
+        config().namingConvention(new InfluxNamingConvention());
+        this.config = config;
+        this.httpClient = httpClient;
+        start(threadFactory);
+    }
+
+    public static Builder builder(InfluxConfig config) {
+        return new Builder(config);
+    }
+
+    @Override
+    public void start(ThreadFactory threadFactory) {
+        if (config.enabled()) {
+            logger.info("publishing metrics to influx every " + TimeUtils.format(config.step()));
+        }
+        super.start(threadFactory);
+    }
+
+    private void createDatabaseIfNecessary() {
+        if (!config.autoCreateDb() || databaseExists)
+            return;
+
+        try {
+            String createDatabaseQuery = new CreateDatabaseQueryBuilder(config.db()).setRetentionDuration(config.retentionDuration())
+                    .setRetentionPolicyName(config.retentionPolicy())
+                    .setRetentionReplicationFactor(config.retentionReplicationFactor())
+                    .setRetentionShardDuration(config.retentionShardDuration()).build();
+
+            httpClient
+                    .post(config.uri() + "/query?q=" + URLEncoder.encode(createDatabaseQuery, "UTF-8"))
+                    .withBasicAuthentication(config.userName(), config.password())
+                    .send()
+                    .onSuccess(response -> {
+                        logger.debug("influx database {} is ready to receive metrics", config.db());
+                        databaseExists = true;
+                    })
+                    .onError(response -> logger.error("unable to create database '{}': {}", config.db(), response.body()));
+        } catch (Throwable e) {
+            logger.error("unable to create database '{}'", config.db(), e);
+        }
+    }
+
+    @Override
+    protected void publish() {
+        createDatabaseIfNecessary();
+
+        try {
+            String influxEndpoint = config.uri() + "/write?consistency=" + config.consistency().toString().toLowerCase() + "&precision=ms&db=" + config.db();
+            if (StringUtils.isNotBlank(config.retentionPolicy())) {
+                influxEndpoint += "&rp=" + config.retentionPolicy();
+            }
+
+            for (List<Meter> batch : MeterPartition.partition(this, config.batchSize())) {
+                httpClient.post(influxEndpoint)
+                        .withBasicAuthentication(config.userName(), config.password())
+                        .withPlainText(batch.stream()
+                                .flatMap(m -> m.match(
+                                        gauge -> writeGauge(gauge.getId(), gauge.value()),
+                                        counter -> writeCounter(counter.getId(), counter.count()),
+                                        this::writeTimer,
+                                        this::writeSummary,
+                                        this::writeLongTaskTimer,
+                                        gauge -> writeGauge(gauge.getId(), gauge.value(getBaseTimeUnit())),
+                                        counter -> writeCounter(counter.getId(), counter.count()),
+                                        this::writeFunctionTimer,
+                                        this::writeMeter))
+                                .collect(joining("\n")))
+                        .compressWhen(config::compressed)
+                        .send()
+                        .onSuccess(response -> {
+                            logger.debug("successfully sent {} metrics to InfluxDB.", batch.size());
+                            databaseExists = true;
+                        })
+                        .onError(response -> logger.error("failed to send metrics to influx: {}", response.body()));
+            }
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("Malformed InfluxDB publishing endpoint, see '" + config.prefix() + ".uri'", e);
+        } catch (Throwable e) {
+            logger.error("failed to send metrics to influx", e);
+        }
+    }
+
+    // VisibleForTesting
+    Stream<String> writeMeter(Meter m) {
+        List<Field> fields = new ArrayList<>();
+        for (Measurement measurement : m.measure()) {
+            double value = measurement.getValue();
+            if (!Double.isFinite(value)) {
+                continue;
+            }
+            String fieldKey = measurement.getStatistic().getTagValueRepresentation()
+                    .replaceAll("(.)(\\p{Upper})", "$1_$2").toLowerCase();
+            fields.add(new Field(fieldKey, value));
+        }
+        if (fields.isEmpty()) {
+            return Stream.empty();
+        }
+        Meter.Id id = m.getId();
+        return Stream.of(influxLineProtocol(id, id.getType().name().toLowerCase(), fields.stream()));
+    }
+
+    private Stream<String> writeLongTaskTimer(LongTaskTimer timer) {
+        Stream<Field> fields = Stream.of(
+                new Field("active_tasks", timer.activeTasks()),
+                new Field("duration", timer.duration(getBaseTimeUnit()))
+        );
+        return Stream.of(influxLineProtocol(timer.getId(), "long_task_timer", fields));
+    }
+
+    // VisibleForTesting
+    Stream<String> writeCounter(Meter.Id id, double count) {
+        if (Double.isFinite(count)) {
+            return Stream.of(influxLineProtocol(id, "counter", Stream.of(new Field("value", count))));
+        }
+        return Stream.empty();
+    }
+
+    // VisibleForTesting
+    Stream<String> writeGauge(Meter.Id id, Double value) {
+        if (Double.isFinite(value)) {
+            return Stream.of(influxLineProtocol(id, "gauge", Stream.of(new Field("value", value))));
+        }
+        return Stream.empty();
+    }
+
+    private Stream<String> writeFunctionTimer(FunctionTimer timer) {
+        Stream<Field> fields = Stream.of(
+                new Field("sum", timer.totalTime(getBaseTimeUnit())),
+                new Field("count", timer.count()),
+                new Field("mean", timer.mean(getBaseTimeUnit()))
+        );
+
+        return Stream.of(influxLineProtocol(timer.getId(), "histogram", fields));
+    }
+
+    private Stream<String> writeTimer(Timer timer) {
+        final Stream<Field> fields = Stream.of(
+                new Field("sum", timer.totalTime(getBaseTimeUnit())),
+                new Field("count", timer.count()),
+                new Field("mean", timer.mean(getBaseTimeUnit())),
+                new Field("upper", timer.max(getBaseTimeUnit()))
+        );
+
+        final Stream<Field> totalFields = writePercentiles(timer, fields);
+        return Stream.of(influxLineProtocol(timer.getId(), "histogram", totalFields));
+    }
+
+    private Stream<String> writeSummary(DistributionSummary summary) {
+        final Stream<Field> fields = Stream.of(
+                new Field("sum", summary.totalAmount()),
+                new Field("count", summary.count()),
+                new Field("mean", summary.mean()),
+                new Field("upper", summary.max())
+        );
+
+        final Stream<Field> totalFields = writePercentiles(summary, fields);
+        return Stream.of(influxLineProtocol(summary.getId(), "histogram", totalFields));
+    }
+
+    private Stream<Field> writePercentiles(HistogramSupport histogramSupport, Stream<Field> fields) {
+        Stream<Field> totalFields;
+        final ValueAtPercentile[] percentileValues = histogramSupport.takeSnapshot().percentileValues();
+        if (percentileValues.length > 0) {
+            List<Field> quantiles = new ArrayList<>(percentileValues.length);
+            for (ValueAtPercentile v : percentileValues) {
+                quantiles.add(new Field("phi" + v.percentile(), v.value(getBaseTimeUnit())));
+
+            }
+            totalFields = Stream.concat(fields, quantiles.stream());
+        } else {
+            totalFields = fields;
+        }
+        return totalFields;
+    }
+
+    private String influxLineProtocol(Meter.Id id, String metricType, Stream<Field> fields) {
+        String tags = getConventionTags(id).stream()
+                .filter(t -> StringUtils.isNotBlank(t.getValue()))
+                .map(t -> "," + t.getKey() + "=" + t.getValue())
+                .collect(joining(""));
+
+        return getConventionName(id)
+                + tags + ",metric_type=" + metricType + " "
+                + fields.map(Field::toString).collect(joining(","))
+                + " " + clock.wallTime();
+    }
+
+    @Override
+    protected final TimeUnit getBaseTimeUnit() {
+        return TimeUnit.MILLISECONDS;
+    }
+
+    public static class Builder {
+        private final InfluxConfig config;
+
+        private Clock clock = Clock.SYSTEM;
+        private ThreadFactory threadFactory = DEFAULT_THREAD_FACTORY;
+        private HttpSender httpClient;
+
+        @SuppressWarnings("deprecation")
+        Builder(InfluxConfig config) {
+            this.config = config;
+            this.httpClient = new HttpUrlConnectionSender(config.connectTimeout(), config.readTimeout());
+        }
+
+        public Builder clock(Clock clock) {
+            this.clock = clock;
+            return this;
+        }
+
+        public Builder threadFactory(ThreadFactory threadFactory) {
+            this.threadFactory = threadFactory;
+            return this;
+        }
+
+        public Builder httpClient(HttpSender httpClient) {
+            this.httpClient = httpClient;
+            return this;
+        }
+
+        public InfluxMeterRegistry build() {
+            return new InfluxMeterRegistry(config, clock, threadFactory, httpClient);
+        }
+    }
+
+    class Field {
+        final String key;
+        final double value;
+
+        Field(String key, double value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return key + "=" + DoubleFormat.decimalOrNan(value);
+        }
+    }
+
+}
